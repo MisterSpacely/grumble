cfae00252fcba5e976c8ae81c8bebc7564b0eb55
jwboykin <jwboykin@ncsu.edu>
2020-04-04 13:27:34 -0400

    Revert "use default completer if custom complter returns nil"
    This reverts commit 20655948b1f988045758be035cf7de583ac7c0ed.
    
    # Conflicts:
    #	completer.go

--- completer.go
+++ completer.go
@@ -81,14 +81,11 @@ func (c *completer) Do(line []rune, pos int) (newLine [][]rune, length int) {
 
 		// Call the custom completer if present.
 		if cmd.Completer != nil {
-			tempWords := cmd.Completer(prefix, rest) //netgrumble change, if completer returns nil use default completer
-			if tempWords != nil {
-				words = tempWords
-				for _, w := range words {
-					suggestions = append(suggestions, []rune(strings.TrimPrefix(w, prefix)))
-				}
-				return suggestions, len(prefix)
+			words = cmd.Completer(prefix, rest)
+			for _, w := range words {
+				suggestions = append(suggestions, []rune(strings.TrimPrefix(w, prefix)))
 			}
+			return suggestions, len(prefix)
 		}
 
 		// No rest must be there.
